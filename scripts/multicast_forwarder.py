f32060ddabc43e99dbe19d419db42a12a0bec7fd
zanderdenning <zrdenning@gmail.com>
2024-10-05 16:39:42 -0700

    Mac multicast script

--- scripts/multicast_forwarder.py
+++ scripts/multicast_forwarder.py
@@ -1,11 +1,16 @@
 import socket
 import struct
+import sys
+
+if len(sys.argv) < 2:
+	print("Missing IP")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 128)
 sock.bind(("", 42080))
 group = socket.inet_aton("224.0.0.3")
-host = socket.inet_aton(socket.gethostbyname(socket.gethostname()))
+# host = socket.inet_aton(socket.gethostbyname(socket.gethostname()))
+host = socket.inet_aton("10.0.0.%s" % sys.argv[1])
 # sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, host)
 mreq = struct.pack("4sL", group, socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, group + host)
@@ -18,8 +23,8 @@ sock_localhost = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO
 while True:
 	try:
 		data, addr = sock.recvfrom(1024)
-		packet = len(addr[0]).to_bytes(1) + addr[0].encode() + data
-		print(packet)
+		packet = len(addr[0]).to_bytes(1, "little") + addr[0].encode() + data
+		# print(packet)
 		sock_localhost.sendto(packet, ("127.0.0.1", 42099))
 	except Exception as e:
 		# print(e)
