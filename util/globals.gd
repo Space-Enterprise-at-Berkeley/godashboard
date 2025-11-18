22a51a4388e08f12e5d3f8e451360bed4ca84a12
zahedsiddiqui <zsidd81@gmail.com>
2025-11-17 19:25:01 -0800

    Abort names show up on dashboard now (yay)

--- util/globals.gd
+++ util/globals.gd
@@ -10,12 +10,18 @@ func _ready() -> void:
 	add_child(heartbeat_timer)
 	heartbeat_timer.timeout.connect(_heartbeat)
 	heartbeat_timer.start()
-	Databus.register_callback("abortReason", null, _handle_packet)
+	Databus.register_callback("abortReason", null, _handle_abort_packet)
 
 func _heartbeat() -> void:
 	Databus.validate_and_send("bcast", "Heartbeat", {
 		"packetSpecVersion": Config.config["protoConfig"]["version"]
 	}, true)
 
-func _handle_packet(value: Variant, timestamp: int) -> void:
-	GoLogger.warn("Abort reason: %s" % value)
+func _handle_abort_packet(value: Variant, timestamp: int) -> void:
+	var abort_name: String = "";
+	var abort_enums: Dictionary = Config.config["reverse_enum_lookup"]["AbortCode"];
+	if abort_enums.has(value):
+		abort_name = abort_enums[value];
+	else:
+		abort_name = "[UNKNOWN ABORTCODE]";
+	GoLogger.warn("Abort code %s: %s" % [value, abort_name]);
