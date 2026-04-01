0d6253665dbad1a3f2fa0b2750f09309c807b814
Lin Jiang <jianglin@lnjng.com>
2026-03-31 23:48:13 -0700

    add actuation counters

--- display/squares/six_value_square/six_value_square_field.gd
+++ display/squares/six_value_square/six_value_square_field.gd
@@ -8,6 +8,9 @@ var field: String = ""
 var units: String = ""
 var name_text: String = ""
 var last_timestamp: int = 0
+var mode: String = "value"    # value or counter
+var count: int = 0
+var last_value: Variant = null
 
 func setup(config: Dictionary, is_null: bool) -> void:
 	if is_null:
@@ -18,8 +21,13 @@ func setup(config: Dictionary, is_null: bool) -> void:
 	units = config["units"]
 	name_text = name
 	name_label.text = config["name"]
-	Databus.register_callback(field, get_window(), _handle_packet)
-	update_field(0.0)
+	mode = config.get("mode", "value")
+	if mode == "counter":
+		Databus.register_callback(field, get_window(), _handle_packet_counter)
+		update_field(0)
+	else:
+		Databus.register_callback(field, get_window(), _handle_packet)
+		update_field(0.0)
 
 func _handle_packet(value: Variant, timestamp: int) -> void:
 	if abs(timestamp - last_timestamp) <= 100:
@@ -27,5 +35,12 @@ func _handle_packet(value: Variant, timestamp: int) -> void:
 	update_field(value)
 	last_timestamp = timestamp
 
-func update_field(value: float) -> void:
-	value_label.text = str(value).pad_decimals(1) + " " + units
+func _handle_packet_counter(value: Variant, timestamp: int) -> void:
+	if last_value == value:
+		return
+	count = count + 1
+	update_field(count, 0)
+	last_value = value
+
+func update_field(value: float, decimals: int = 1) -> void:
+	value_label.text = str(value).pad_decimals(decimals) + " " + units
